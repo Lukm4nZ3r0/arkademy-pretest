b8df1090091df4ebcc6c7b74249eaa12db52ae20
Lukm4nZ3r0 <43030283+Lukm4nZ3r0@users.noreply.github.com>
2019-06-15 23:19:23 +0700

    Add files via upload

---
+++ 6.js
@@ -0,0 +1,19 @@
+function binaryConverter(desimal=12){
+    var hasil = []
+
+    for(desimal ; desimal>0 ; desimal/2){
+        if(desimal!==0){
+            if(desimal%2!==0){
+                desimal = Math.floor(desimal/2)
+                hasil.push(0)
+            }
+            else{
+                desimal = Math.floor(desimal/2)
+                hasil.push(1)
+            }
+        }
+    }
+
+    return hasil
+}
+console.log(binaryConverter())
