6ede53fdd149f5cec6a3ab45d1328297d591ebab
Lukm4nZ3r0 <43030283+Lukm4nZ3r0@users.noreply.github.com>
2019-06-15 22:39:20 +0700

    Update 3.js

--- 3.js
+++ 3.js
@@ -2,7 +2,10 @@ function hitungTinggiPohon(tinggiAwal,tahun){
     var hasil
     if(tinggiAwal>0 && tahun>0){
         // dikali 2 lalu ditambah 1
-        hasil = ((tinggiAwal*2)+1)*tahun
+        hasil = ((tinggiAwal*2)+1)
+        for(var i = 0 ; i<tahun-1 ; i++){
+            hasil = ((hasil*2)+1)
+        }
     }
 
     return hasil+' meter'
