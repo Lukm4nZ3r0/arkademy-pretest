efb8ef12bc88b25c4914797e184baea1a448ebfa
Lukm4nZ3r0 <43030283+Lukm4nZ3r0@users.noreply.github.com>
2019-06-16 06:28:47 +0700

    Update 5.js
    menambah keterangan

--- 5.js
+++ 5.js
@@ -1,16 +1,16 @@
 function palindrome(text = 'banananana',findText = 'nana'){
     var hasil = 0
 
-    var textLength = text.length
-    var findTextLength = findText.length
+    var textLength = text.length // klo valuenya default, textLength = 10
+    var findTextLength = findText.length // klo valuenya default, findTextLength = 4
 
-    for(var a = 0 ; a<textLength-1 ; a++){
+    for(var a = 0 ; a<textLength-1 ; a++){ // hitung dari kiri ke kanan
         var textSubString = text.substring(a,findTextLength+a)
         if(textSubString == findText){
             hasil++
         }
     }
-    for(var a = textLength ; a>0 ; a--){
+    for(var a = textLength ; a>0 ; a--){ // hitung dari kanan ke kiri
         var textSubString = text.substring(a,findTextLength+a)
         if(textSubString == findText){
             hasil++
